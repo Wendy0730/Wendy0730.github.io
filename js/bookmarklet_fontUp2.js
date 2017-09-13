222cc8234c035c24d2262c6f6e5eb88ca3e11e40
Wendy0730 <31809844+Wendy0730@users.noreply.github.com>
2017-09-13 09:02:53 -0700

    font-up 7

--- js/bookmarklet_fontUp2.js
+++ js/bookmarklet_fontUp2.js
@@ -31,12 +31,8 @@ function initMyBookmarklet(){
     (window.myBookmarklet = function (){
 
         //YOUR CODE GOES HERE!
-       var p=document.getElementsByTagName('*');
-        for(i=0;i<p.length;i++){
-            if(p[i].style.fontSize){
-                var s=parseInt(p[i].style.fontSize.replace("px",""));
-            } else {
-                var s=12;}s+=2;p[i].style.fontSize=s+"px"}
+       avascript:void(document.body.style.background=prompt('What color would you like the background to be?'));
+
 
     })();
 
